8cab05802de1d864fed24c7db65e1f59554b9aa0
Jack <victor2015yhm@gmail.com>
2022-10-20 12:19:26 +0800

    【关联单号】US202209130700 【修改类型】Optimize 【提交描述】优化删除多余注解

--- src/main/java/cn/yhm/developer/ecology/model/response/ResultResponse.java
+++ src/main/java/cn/yhm/developer/ecology/model/response/ResultResponse.java
@@ -3,7 +3,6 @@ package cn.yhm.developer.ecology.model.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.Accessors;
 
 /**
  * 结果抽象响应类
@@ -11,7 +10,6 @@ import lombok.experimental.Accessors;
  * @author victor2015yhm@gmail.com
  * @since 2022-10-17 07:23:46
  */
-@Accessors(chain = true)
 @Setter
 @Getter
 public abstract class ResultResponse<T> implements GatewayResponse {
--- src/main/java/cn/yhm/developer/ecology/model/response/ResultsResponse.java
+++ src/main/java/cn/yhm/developer/ecology/model/response/ResultsResponse.java
@@ -3,7 +3,6 @@ package cn.yhm.developer.ecology.model.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.Accessors;
 
 import java.util.List;
 
@@ -13,7 +12,6 @@ import java.util.List;
  * @author victor2015yhm@gmail.com
  * @since 2022-10-17 07:23:46
  */
-@Accessors(chain = true)
 @Setter
 @Getter
 public abstract class ResultsResponse<T> implements GatewayResponse {
